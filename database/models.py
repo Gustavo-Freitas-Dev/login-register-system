60ee87ad0bf848b208daef3aae42b4d77f41821f
DATA USER <data.user@DAFF.LOCAL>
2025-05-29 08:34:14 -0300

    Testando a funcionalidade de verificar login

--- database/models.py
+++ database/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Column, Integer, String, Date, Boolean, DateTime
 from datetime import datetime
 from passlib.hash import bcrypt
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from database.connection import Base, session
 
 class Usuario(Base):
@@ -25,6 +28,17 @@ class Usuario(Base):
 
 #Testando a aplicação 
 if __name__ == '__main__':
-    adicionar_conta = Usuario('Teste Testando2', '28/02/2012', 'testetestestando@gmail.com', 'teste123@')
-    session.add(adicionar_conta)
-    session.commit()
+    # adicionar_conta = Usuario('Teste da Silva', '28/02/2012', 'testedasilva@gmail.com', 'silvinha123@')
+    # session.add(adicionar_conta)
+    # session.commit()
+
+    # Verificador de usuário
+    email = str(input('E-mail: '))
+    senha = str(input('Senha: '))
+
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+
+    if usuario and bcrypt.verify(senha, usuario.senha):
+        print('Usuário cadastrado!!')
+    else:
+        print('Novo usuário cadastrado!!!!')
